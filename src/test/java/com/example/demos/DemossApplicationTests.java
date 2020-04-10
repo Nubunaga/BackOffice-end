64739d81e8e9d27a8effe0ba2b3fbff5f190b6b7
Netanel <netanel@kth.se>
2020-04-10 13:08:45 +0200

    help

--- src/test/java/com/example/demos/DemossApplicationTests.java
+++ src/test/java/com/example/demos/DemossApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demos;
 
 import static org.junit.Assert.*;
 
+import com.example.demos.model.User;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
