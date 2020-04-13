3fd39c01e2b86091156f6299f2015c801d25d7cf
Netanel <netanel@kth.se>
2020-04-13 22:17:41 +0200

    added some tests

--- src/test/java/com/example/demos/UserTest.java
+++ src/test/java/com/example/demos/UserTest.java
@@ -4,21 +4,37 @@ import com.example.demos.db.Users;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 
-
-
+@ContextConfiguration(classes = Users.class)
 @SpringBootTest
 class UserTest {
-	private Users users;
+	@Autowired
+	private Users users = new Users();
+
 	@Test
-	void contextLoads() {
-		users = new Users();
+	public void contexLoads() throws Exception {
+		assertThat(users).isNotNull();
+	}
+
+	@Test
+	void nameTest() {
 		users.setnewUser("Netanel", "mail@mail.com", "123");
-		
 		assertTrue("The name is not correct", "Netanel" == users.getName());
-		assertTrue("The email is not correct", "mail@mail.com" ==users.getEmail());
-		assertTrue("The pass is not correct", "123" == users.getPassword());
+	}
+	@Test
+	void emailTest() {
+		users.setnewUser("Netanel", "mail@mail.com", "123");
+		assertTrue("The name is not correct", "mail@mail.com" == users.getEmail());
+	}
+	@Test
+	void passTest() {
+		users.setnewUser("Netanel", "mail@mail.com", "123");
+		assertTrue("The name is not correct", "123" == users.getPassword());
 	}
 }
