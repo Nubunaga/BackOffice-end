29efc53236dd02ec7f6a078d236412e165420f43
Netanel <netanel@kth.se>
2020-04-15 07:53:46 +0200

    more testing of env

--- src/main/java/com/example/demos/DemossApplication.java
+++ src/main/java/com/example/demos/DemossApplication.java
@@ -2,11 +2,15 @@ package com.example.demos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(confProp.class)
 public class DemossApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DemossApplication.class, args);
 	}
 
