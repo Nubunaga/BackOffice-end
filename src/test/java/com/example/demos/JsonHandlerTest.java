4df919ea18d849fe5da08101874a317650112261
Netanel <netanel@kth.se>
2020-04-20 15:44:09 +0200

    added a testJson filé

--- src/test/java/com/example/demos/JsonHandlerTest.java
+++ src/test/java/com/example/demos/JsonHandlerTest.java
@@ -1,10 +1,27 @@
 package com.example.demos;
 
+import com.example.demos.model.JsonHandler;
+import com.example.demos.utils.Config;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = Config.class) 
+@ContextConfiguration(classes = Config.class)
 public class JsonHandlerTest {
+    JsonHandler jsonHandler = new JsonHandler(); 
 
+    @Test
+    public void contextLoad()throws Exception{
+        assertTrue("Jsonhandler object is not made", jsonHandler != null);
+    }
+
+
+    @Test
+    public void newOrderTest(){
+    
+    }
 }
