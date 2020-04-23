13e77411cf9bcf9d7b4cc564b0a113837516a730
Netanel <netanel@kth.se>
2020-04-23 10:49:28 +0200

    fixed

--- src/test/java/com/example/demos/ControllerAdvertisment.java
+++ src/test/java/com/example/demos/ControllerAdvertisment.java
@@ -1,9 +1,15 @@
 package com.example.demos;
 
+import com.example.demos.utils.Config;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = Config.class)
-@WebMvcTest(controllers = ControllerUser.class)
+@WebMvcTest(controllers = ControllerAdvertisment.class)
 public class ControllerAdvertisment {
 
 }
